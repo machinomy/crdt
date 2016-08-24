58e366b60e4e517cdbb734730f56f05298a39a3c
Sergey Ukustov <sergey@ukstv.me>
2016-08-24 17:00:29 +0300

    Removed unused code

--- src/main/scala/com/machinomy/crdt/op/PartialOrderDag.scala
+++ src/main/scala/com/machinomy/crdt/op/PartialOrderDag.scala
@@ -26,10 +26,8 @@ case class PartialOrderDag[V, E[X] <: DiEdgeLikeIn[X]](vertices: TPSet[V], edges
     }
   }
 
-  def remove(v: V): PartialOrderDag.UpdateResult[V, E] = {
-    println(s"Currently: $value")
-    println(s"Contains: ${value.contains(v)}")
-    println(s"Is Sentinel: ${graphLike.isSentinel(v)}")
+  // @todo This does not work.
+  def remove(v: V): PartialOrderDag.UpdateResult[V, E] =
     if (value.contains(v) && !graphLike.isSentinel(v)) {
       val nextVertices = vertices - v
       val next = new PartialOrderDag[V, E](nextVertices, edges)
@@ -37,7 +35,6 @@ case class PartialOrderDag[V, E[X] <: DiEdgeLikeIn[X]](vertices: TPSet[V], edges
     } else {
       (this, None)
     }
-  }
 
   def run(operation: PartialOrderDag.AddVertex[V, E]): (PartialOrderDag[V, E], Option[Update[V, E]]) =
     add(operation.v, operation.left, operation.right)
